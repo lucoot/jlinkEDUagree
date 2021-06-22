4ceba9e60f06380efd9a40db2ad32aa4817bdced
lucoot <lucoot@gmail.com>
2021-06-22 11:04:49 -0700

    Added support for J-Link Mini EDU

--- jlinkEDUagreeWIN.bat
+++ jlinkEDUagreeWIN.bat
@@ -1,6 +1,6 @@
 ::Author: Lucas C. Jun 2021
 ::Description:
-::Disables the J-Link EDU License Agreement Pop-Up.
+::Disables the J-Link EDU & Mini EDU License Agreement Pop-Ups.
 ::Updates the date in the J-Link settings Registry.
 ::Installation
 ::Use Windows Task Scheduler to run every day automatically at midnight or at login.
@@ -16,3 +16,4 @@ for /f %%i in ('powershell -command "'{0:x2}' -f" %mon%') do set new=%new%%%i
 for /f %%i in ('powershell -command "'{0:x2}' -f" %day%') do set new=%new%%%i
 ::Replace old value with the new value
 reg add "HKEY_CURRENT_USER\Software\SEGGER\J-Link" /f /v "LicenseEDU_DontShowAgainToday" /t REG_DWORD /d "0x%new%"
+reg add "HKEY_CURRENT_USER\Software\SEGGER\J-Link" /f /v "LicenseEDUMini_DontShowAgainToday" /t REG_DWORD /d "0x%new%"
